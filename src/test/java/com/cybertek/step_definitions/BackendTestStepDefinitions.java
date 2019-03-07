1952a726320e2ee3efa637867e9b63148008a841
github@cybertekschool.com <github@cybertekschool.com>
2019-03-06 19:57:49 -0500

    added compare tables test

--- src/test/java/com/cybertek/step_definitions/BackendTestStepDefinitions.java
+++ src/test/java/com/cybertek/step_definitions/BackendTestStepDefinitions.java
@@ -3,6 +3,7 @@ package com.cybertek.step_definitions;
 import com.cybertek.pages.MyTeamPage;
 import com.cybertek.utilities.BrowserUtils;
 import com.cybertek.utilities.DatabaseUtility;
+import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import org.junit.Assert;
 
@@ -20,7 +21,7 @@ public class BackendTestStepDefinitions {
         // build the sql based on the team name
         String sql = "select u.firstname " +
                 " from users u join team t on t.id = " +
-                "u.team_id and t.name = '"+teamName+"';";
+                "u.team_id and t.name = '" + teamName + "';";
         // getColumnData returns data of a single column
         List<Object> names = DatabaseUtility.getColumnData(sql, "firstname");
 
@@ -36,5 +37,35 @@ public class BackendTestStepDefinitions {
 
 
     }
+
+    List<Object> clusterTable;
+
+    @Given("there is a list of clusters in the clusters table")
+    public void there_is_a_list_of_clusters_in_the_clusters_table() {
+        // get the list of id from the cluster table
+        String sql = "select * from cluster;";
+        clusterTable = DatabaseUtility.getColumnData(sql, "id");
+        System.out.println(clusterTable);
+    }
+
+    List<Object> roomTable;
+
+    @Given("all rooms have a clusters id in room table")
+    public void all_rooms_have_a_clusters_id_in_room_table() {
+        // get the list of cluster_id from tjeh room table
+        String sql = "select * from room;";
+        roomTable = DatabaseUtility.getColumnData(sql, "cluster_id");
+        System.out.println(roomTable);
+    }
+
+
+    @Then("all the clusters ids must be from the clusters table")
+    public void all_the_clusters_ids_must_be_from_the_clusters_table() {
+        // compare
+        for (Object o : roomTable) {
+            Assert.assertTrue(clusterTable.contains(o));
+        }
+    }
+
 
 }
--- src/test/resources/features/team_info_back_end_test.feature
+++ src/test/resources/features/team_info_back_end_test.feature
@@ -1,7 +1,13 @@
 Feature: Team information back end
-  @wip @db
+ @db
   Scenario: Team members db test
     Given user on the login page
     When user logs in using "mstacey8r@imdb.com" and "skylargiblin"
     When the user goes to the "my team" page
     Then team information should match database records
+
+  @wip @db
+  Scenario: Verify rooms and clusters
+    Given there is a list of clusters in the clusters table
+    And all rooms have a clusters id in room table
+    Then all the clusters ids must be from the clusters table
